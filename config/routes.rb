b8e05ebb11789737d7bc12c6ce8be87cac43c6a4
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-02-15 22:26:00 +0000

    made path to destroy/delete photo

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ get("/", {:controller => "users", :action => "index"})
 get("/users/:path_username", {:controller =>"users", :action =>"show"})
 
 get("/photos", { :controller => "photos", :action =>"index"})
+get("/photos/:path_id", { :controller => "photos", :action => "show"})
+get("/delete_photo/:path_id", {:controller => "photos", :action => "destroy"})
 
 end
